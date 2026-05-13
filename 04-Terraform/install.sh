fa90c707f3d299bcb43b6c314f6aead4b80e8f98
Murat Kaynar <muratkaynar73@hotmail.com>
2026-05-13 09:03:42 +0300

    install.sh GPG key updated

--- 04-Terraform/install.sh
+++ 04-Terraform/install.sh
@@ -6,15 +6,15 @@ apt-get update -y
 apt-get install -y docker.io
 systemctl start docker
 systemctl enable docker
-usermod -aG docker ubuntu # Ubuntu kullanıcısına Docker yetkisi veriyoruz
+usermod -aG docker ubuntu
 
 # --- JENKINS KURULUMU ---
-# Jenkins Java tabanlıdır, bu yüzden önce Java 17 kuruyoruz
+# Jenkins Java tabanlıdır
 apt-get install -y fontconfig openjdk-17-jre
 
-# Jenkins'in resmi repolarını sistemimize ekliyoruz
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+# Güncel Jenkins GPG Anahtarı ve Repo Tanımı (Senin eklediğin doğru kısım)
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 # Repoları güncelleyip Jenkins'i kuruyoruz
 apt-get update -y
